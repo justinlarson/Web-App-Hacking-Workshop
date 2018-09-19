74a13240512c1f9220ede00af5ad46c8adb23055
phant0mtrav3ler <slimlarson@gmail.com>
2018-09-19 13:41:14 -0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+!#/bin/bash
+echo "This won't work if you don't have git, docker and ruby installed, I didn't want to make it smart enough to check"
+git clone git@github.com:sqlmapproject/sqlmap.git &
+git clone git@github.com:danielmiessler/SecLists.git &
+git clone git@github.com:nccgroup/autochrome.git &
+ruby autochrome/autochrome.rb
+open ~/Applications/Chromium.app &
+docker run --detach --rm -it -p 80:80 cyrivs89/web-dvws &
+docker pull bkimminich/juice-shop
+docker run --detach --rm -p 3000:3000 bkimminich/juice-shop
