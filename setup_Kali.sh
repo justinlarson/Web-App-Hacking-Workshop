aa74f69e01a9cfb3d180c9f3a88e0a6b3541627e
phant0mtrav3ler <slimlarson@gmail.com>
2018-09-24 22:11:42 -0600

    Update setup_Kali.sh

--- setup_Kali.sh
+++ setup_Kali.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 echo "[!] \n This won't work if you don't have git, docker, Chromium, and ruby installed. I didn't want to make it smart enough to check \n [!]"
+apt-get --qq install python3-pip mysql -y
 git clone git@github.com:justinlarson/Web-App-Hacking-Workshop.git &
 git clone git@github.com:justinlarson/web-app-hacking-workshop-solutions.git &
-
 git clone git@github.com:sqlmapproject/sqlmap.git &
 git clone git@github.com:danielmiessler/SecLists.git &
 git clone git@github.com:nccgroup/autochrome.git &
+git clone git@github.com:sethlaw/vtm.git 
+mysqld &
+mysqladmin -u root create vtmdb
+cd vtm
+pip3 install -r requirements.txt
+python3 manage.py migrate
+python3 manage.py loaddata taskManager/fixtures/*
+python3 manage.py runserver & 
 ruby autochrome/autochrome.rb
 ~/.local/autochrome/chrome &
 docker run --detach --rm -it -p 80:80 cyrivs89/web-dvws &
