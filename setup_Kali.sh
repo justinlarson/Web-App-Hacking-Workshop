27168628ad4220421f8896dfeabc86c435509910
phant0mtrav3ler <slimlarson@gmail.com>
2018-09-24 22:19:56 -0600

    Update setup_Kali.sh

--- setup_Kali.sh
+++ setup_Kali.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 echo "[!] \n This won't work if you don't have git, docker, Chromium, and ruby installed. I didn't want to make it smart enough to check \n [!]"
 apt-get --qq install python3-pip mysql -y
-git clone https://github.com/justinlarson/Web-App-Hacking-Workshop.git &
-git clone https://github.com/justinlarson/web-app-hacking-workshop-solutions.git &
-git clone https://github.com/sqlmapproject/sqlmap.git &
-git clone https://github.com/danielmiessler/SecLists.git &
-git clone https://github.com/nccgroup/autochrome.git &
+# git clone https://github.com/justinlarson/Web-App-Hacking-Workshop.git &
+# git clone https://github.com/justinlarson/web-app-hacking-workshop-solutions.git &
+# git clone https://github.com/sqlmapproject/sqlmap.git &
+# git clone https://github.com/danielmiessler/SecLists.git &
+# git clone https://github.com/nccgroup/autochrome.git &
+# ruby autochrome/autochrome.rb
 git clone https://github.com/sethlaw/vtm.git 
 mysqld &
 mysqladmin -u root create vtmdb
@@ -14,9 +15,6 @@ pip3 install -r requirements.txt
 python3 manage.py migrate
 python3 manage.py loaddata taskManager/fixtures/*
 python3 manage.py runserver & 
-cd ..
-ruby autochrome/autochrome.rb
-~/.local/autochrome/chrome &
-docker run --detach --rm -it -p 80:80 cyrivs89/web-dvws &
-docker pull bkimminich/juice-shop
-docker run --detach --rm -p 3000:3000 bkimminich/juice-shop
+# docker run --detach --rm -it -p 80:80 cyrivs89/web-dvws &
+# docker pull bkimminich/juice-shop
+# docker run --detach --rm -p 3000:3000 bkimminich/juice-shop
