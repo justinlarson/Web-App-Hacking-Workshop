65eaac25f1205b2ba7991569fc8978ca9ccf9bae
phant0mtrav3ler <slimlarson@gmail.com>
2018-09-24 21:31:15 -0600

    Create setup_Kali.sh

---
+++ setup_Kali.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "[!] \n This won't work if you don't have git, docker, Chromium, and ruby installed. I didn't want to make it smart enough to check \n [!]"
+git clone git@github.com:justinlarson/Web-App-Hacking-Workshop.git &
+git clone git@github.com:justinlarson/web-app-hacking-workshop-solutions.git &
+
+git clone git@github.com:sqlmapproject/sqlmap.git &
+git clone git@github.com:danielmiessler/SecLists.git &
+git clone git@github.com:nccgroup/autochrome.git &
+ruby autochrome/autochrome.rb
+~/.local/autochrome/chrome &
+docker run --detach --rm -it -p 80:80 cyrivs89/web-dvws &
+docker pull bkimminich/juice-shop
+docker run --detach --rm -p 3000:3000 bkimminich/juice-shop
