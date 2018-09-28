e1c69a7540978ead2df149d635661fc9fb364b84
phant0mtrav3ler <slimlarson@gmail.com>
2018-09-27 20:38:02 -0700

    Update setup_Mac.sh

--- setup_Mac.sh
+++ setup_Mac.sh
@@ -3,7 +3,7 @@ echo "[!] \n This won't work if you don't have brew I didn't want to make it sma
 brew cask install burp-suite &
 brew install git
 git clone https://github.com/justinlarson/web-app-hacking-workshop-solutions.git &
-brew install hydra mysql python3 &
+brew install hydra mysql python3 
 git clone https://github.com/sqlmapproject/sqlmap.git &
 git clone https://github.com/danielmiessler/SecLists.git &
 git clone https://github.com/sethlaw/vtm.git 
