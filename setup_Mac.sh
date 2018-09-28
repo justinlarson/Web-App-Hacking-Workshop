1b835a7c5a24b79f0f5e0f3b82bdd24617b1b724
phant0mtrav3ler <slimlarson@gmail.com>
2018-09-27 21:29:19 -0700

    Update setup_Mac.sh

--- setup_Mac.sh
+++ setup_Mac.sh
@@ -3,16 +3,16 @@ echo "[!] \n This won't work if you don't have brew I didn't want to make it sma
 #brew cask install burp-suite &
 brew install git
 git clone https://github.com/justinlarson/web-app-hacking-workshop-solutions.git &
-brew install hydra mysql python3 
+brew install hydra 
 git clone https://github.com/sqlmapproject/sqlmap.git &
 git clone https://github.com/danielmiessler/SecLists.git &
-git clone https://github.com/sethlaw/vtm.git 
-brew services start mysql
-mysqladmin -u root create vtmdb
-pip3.7 install -r vtm/requirements.txt
-python3 vtm/manage.py migrate
-python3 vtm/manage.py loaddata taskManager/fixtures/*
-python3 vtm/manage.py runserver & 
-docker run --detach --rm -it -p 80:80 cyrivs89/web-dvws &
+#git clone https://github.com/sethlaw/vtm.git 
+#brew services start mysql
+#mysqladmin -u root create vtmdb
+#pip3.7 install -r vtm/requirements.txt
+#python3 vtm/manage.py migrate
+#python3 vtm/manage.py loaddata taskManager/fixtures/*
+#python3 vtm/manage.py runserver & 
+#docker run --detach --rm -it -p 80:80 cyrivs89/web-dvws &
 docker pull bkimminich/juice-shop
 docker run --detach --rm -p 3000:3000 bkimminich/juice-shop
